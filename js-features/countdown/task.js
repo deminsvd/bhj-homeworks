c4120f202a1ee3ded056f9b7a046a02e2c0017c6
Sergey Demin <deminsv@gmail.com>
2025-07-01 10:43:44 +0300

    first update commit

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,7 +1,8 @@
 const p = document.getElementById('timer')
-setInterval(() => {
+let timer = setInterval(() => {
     p.textContent = Number(p.textContent) - 1
     if (p.textContent == 0) {
     alert('Вы победили в конкурсе!')
+    clearTimeout(timer)
 }
 }, 1000)
